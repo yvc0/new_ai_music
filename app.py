f81e2df6caabc43feb2cdf9d92ec9acba9db01c6
yvc0 <88483007+yvc0@users.noreply.github.com>
2025-10-07 18:54:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -507,4 +507,5 @@ app = create_app()
 
 if __name__ == "__main__":
     # Local dev run
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
